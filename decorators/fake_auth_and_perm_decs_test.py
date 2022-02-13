36cba28169a446ee39ebd0f369117656adfee279
Anton Mardanov <Lotrus@yandex.ru>
2022-02-13 21:15:55 +0300

    Add files via upload

---
+++ decorators/fake_auth_and_perm_decs_test.py
@@ -0,0 +1,39 @@
+import unittest
+from decorators.fake_auth_and_perm_decs import (
+    run_action,
+    AuthorizationException,
+    PermissionException,
+)
+
+
+class MyTestCase(unittest.TestCase):
+    def test_ok(self):
+        request = {"login": "Petya", "password": "123", "has_permission": True}
+        self.assertEqual(run_action(request), "Секретная информация")
+
+    def test_bad_login(self):
+        request = {"login": "Pety", "password": "123", "has_permission": True}
+        with self.assertRaises(AuthorizationException):
+            run_action(request)
+
+    def test_bad_pass(self):
+        request = {
+            "login": "Petya",
+            "password": "1234",
+            "has_permission": True,
+        }
+        with self.assertRaises(AuthorizationException):
+            run_action(request)
+
+    def test_no_permission(self):
+        request = {
+            "login": "Petya",
+            "password": "123",
+            "has_permission": False,
+        }
+        with self.assertRaises(PermissionException):
+            run_action(request)
+
+
+if __name__ == "__main__":
+    unittest.main()
