98b6cf28ef0211fce3bb3490960e4fcb5bd0bd92
Anton Mardanov <Lotrus@yandex.ru>
2022-02-13 22:46:47 +0300

    Update garbage_collector.py

--- garbage_collector.py
+++ garbage_collector.py
@@ -1,4 +1,5 @@
 # https://stackify.com/python-garbage-collection/#:~:text=The%20Python%20garbage%20collector%20has,a%20threshold%20number%20of%20objects.
+# https://towardsdatascience.com/memory-management-and-garbage-collection-in-python-c1cb51d1612c
 # The main garbage collection mechanism in CPython is through reference counts.
 # can't handle cyclic references
 # >>> import sys
